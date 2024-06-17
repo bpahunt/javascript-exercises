12faadf1dd5d0c3a4d80740ec9a760303db019a6
bpahunt <bpahunt@googlemail.com>
2024-06-17 13:05:26 +0100

    Add solution for 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    total = 0
+    if (num1 < 1 || num2 < 1 || typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR"
+    } else {
+        for (let i = (num1 < num2 ? num1 : num2); i <= (num1 > num2 ? num1 : num2); i++) {
+        total += i
+        }
+    }    
+    return total
 };
 
 // Do not edit below this line
